0a87d0b531fabb7c2c54accfb946dca7d46a8276
Hardik Shrestha <hardik.ho@gmail.com>
2025-03-03 12:23:34 -0500

    web

--- App.js
+++ App.js
@@ -80,7 +80,7 @@ function App() {
   };
   
   return (
-    <Router>
+    <Router basename={process.env.PUBLIC_URL}>
       <div className="app">
         {showNotification && (
           <div className="notification">
